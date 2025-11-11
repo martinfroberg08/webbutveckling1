2ec18ddd050c9e8c7e0899f46fbc697eb2d390da
martinfroberg08 <frobergmartin292@gmail.com>
2025-11-11 11:48:16 +0100

    add logo to nav

--- Bobergsgymnasiet/index.js
+++ Bobergsgymnasiet/index.js
@@ -1,9 +1,25 @@
+const nav = document.querySelector("nav");
+const mobileNav = document.querySelector(".mobile-nav");
+const hamburgerBtn = document.querySelector("#menu-open");
+ 
 const closeMenu = () => {
-    const nav = document.querySelector("nav")
-    nav.classList.add("hidden")
+    mobileNav.classList.add("hidden");
+    nav.classList.add("nav-closed");
+    hamburgerBtn.classList.remove("hidden");
+   
 }
-
-
-const menuCloseBtn = document.querySelector(".menu-close-btn")
-menuCloseBtn.addEventListener("click", closeMenu)
-
+ 
+const openMenu = () => {
+    hamburgerBtn.classList.add("hidden");
+    nav.classList.remove("nav-closed");
+    mobileNav.classList.remove("hidden");
+   
+}    
+ 
+const menuCloseBtn = document
+.querySelector("#menu-close")
+.addEventListener("click", closeMenu)
+ 
+const menuOpenBtn = document
+.querySelector("#menu-open")
+.addEventListener("click", openMenu)
